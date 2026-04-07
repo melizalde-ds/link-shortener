dd795b59b5a827a41dfa5c6cd30034c9eeb4e421
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-04-07 13:42:25 -0600

    Fix typo in length constant and simplify encoding logic in Encoder class

--- Utils/Encoder.cs
+++ Utils/Encoder.cs
@@ -1,25 +1,20 @@
-using System.Buffers.Binary;
-using System.Text;
-
 namespace LinkShortener.Utils;
 
 public static class Encoder
 {
     private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private const int Lenght = 5;
+    private const int Length = 5;
 
     public static string GetEncoded(ulong number)
     {
-        var bytes = new byte[8];
-        BinaryPrimitives.WriteUInt64LittleEndian(bytes, number);
-        var base62 = new StringBuilder();
-
-        for (int i = 0; i < Lenght; i++)
+        var result = new char[Length];
+        for (var i = 0; i < Length; i++)
         {
-            var index = bytes[i] % (uint)Chars.Length;
-            base62.Append(Chars[(int)index]);
+            var value = number % (ulong)Chars.Length;
+            result[i] = Chars[(int)value];
+            number /= (ulong)Chars.Length;
         }
 
-        return base62.ToString();
+        return new string(result);
     }
 }
